184512c5ce5a2993ae9920207b2b66ad8d554a50
unknown <www.snowfall@mail.ru>
2021-10-22 21:16:24 +0300

    wtf

---
+++ src/main/java/model/Dog.java
@@ -0,0 +1,18 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dog {
+
+    private String name;
+    private int age;
+
+
+}
---
+++ src/test/java/model/DogTest.java
@@ -0,0 +1,24 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+class DogTest {
+
+    @Test
+    void testGetName() {
+        Dog dog = new Dog("Doggy", 2);
+        
+    }
+
+    @Test
+    void testGetAge() {
+    }
+
+    @Test
+    void testSetName() {
+    }
+
+    @Test
+    void testSetAge() {
+    }
+}
